53a492e3183053940492d84dbea8cc0913e87da8
Kaaz!n <kaazin.splatoon@gmail.com>
2018-05-07 11:34:37 -0400

    Initial commit

---
+++ Assets/1st Party/Scripts/Player/CameraOrbit.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+
+    Transform target;
+
+    public float moveH = 45;
+    public float moveV = 15;
+
+    float currentX;
+    float currentY;
+    public Vector3 offset;
+    public Transform fulcrum;
+    Quaternion currentRot;
+
+    void Awake()
+    {
+        target = GameObject.Find("Player").transform;
+
+    }
+    void Update()
+    {
+
+        transform.position = target.position - offset;
+
+        currentX = Input.GetAxis("Mouse X");
+        currentY = Input.GetAxis("Mouse Y");
+
+
+        
+            MoveVertical();
+          
+            MoveHorizontal();
+
+            ZTarget();
+
+       // transform.LookAt(target);
+
+
+    }
+
+    public void MoveHorizontal()
+    {
+
+        transform.RotateAround(fulcrum.position, Vector3.up, moveH * currentX);
+    }
+
+    public void MoveVertical()
+    {
+        transform.RotateAround(target.position, transform.TransformDirection(Vector3.right), moveV * -currentY);
+    }
+
+    void ZTarget()
+    {
+        if(Input.GetMouseButtonDown(2))
+        {
+            transform.rotation = target.transform.rotation;
+        }
+
+   
+
+    }
+}
---
+++ Assets/1st Party/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3 dir;
+    public Camera cam;
+
+	// Use this for initialization
+	void Awake ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveH = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        if (moveH != 0 || moveV != 0)
+        {
+           // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(dir.x, dir.y, dir.z)), 1);
+        }
+
+        dir = new Vector3(moveH, 0, moveV) * speed * Time.deltaTime;
+        transform.Translate(transform.rotation * new Vector3(-dir.x,dir.y,dir.z));
+    }
+
+  
+}
