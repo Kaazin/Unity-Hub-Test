842382007df666d406da31364ab7edcec57421a7
Kaaz!n <kaazin.splatoon@gmail.com>
2018-05-08 14:11:57 -0400

    added camera movment script

--- Assets/1st Party/Scripts/Player/PlayerMovement.cs
+++ Assets/1st Party/Scripts/Player/PlayerMovement.cs
@@ -18,16 +18,19 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        float moveV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float moveH = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector3 myDir;
+        myDir = cam.transform.forward;
+        myDir.y = 0;
 
-        if (moveH != 0 || moveV != 0)
-        {
-           // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(dir.x, dir.y, dir.z)), 1);
-        }
+        float moveV = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        float moveH = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
 
-        dir = new Vector3(moveH, 0, moveV) * speed * Time.deltaTime;
-        transform.Translate(transform.rotation * new Vector3(-dir.x,dir.y,dir.z));
+        dir = Vector3.Scale(new Vector3(moveH, 0, moveV), myDir)  * speed * Time.deltaTime;
+
+
+        Vector3 currentDir = Vector3.Scale(myDir, dir);
+
+        transform.Translate(dir);
     }
 
   
